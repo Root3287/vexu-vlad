619c6eed6368e3fbfb4b92e9040a788e3cb93837
Timothy Gibbons <tjgibbons10@gmail.com>
2026-01-30 16:05:25 -0600

    chore: Migrated database/db.sql to Sequelize Friendly files

--- db/migrations/20260130212609-create-users.cjs
+++ db/migrations/20260130212609-create-users.cjs
@@ -1,6 +1,7 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
+<<<<<<< HEAD
 	async up(queryInterface, Sequelize) {
 		await queryInterface.createTable('Users', {
 			id: {
@@ -30,4 +31,35 @@ module.exports = {
 	async down(queryInterface, Sequelize) {
 		await queryInterface.dropTable('Users');
 	}
+=======
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+>>>>>>> deba691 (chore: Migrated database/db.sql to Sequelize Friendly files)
 };
--- db/migrations/20260130223000-create-user-tasks.cjs
+++ db/migrations/20260130223000-create-user-tasks.cjs
@@ -1,6 +1,7 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
+<<<<<<< HEAD
 	async up(queryInterface, Sequelize) {
 		await queryInterface.createTable('UserTasks', {
 			id: {
@@ -41,4 +42,46 @@ module.exports = {
 	async down(queryInterface, Sequelize) {
 		await queryInterface.dropTable('UserTasks');
 	}
+=======
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserTasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      task_id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'Tasks',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('UserTasks');
+  }
+>>>>>>> deba691 (chore: Migrated database/db.sql to Sequelize Friendly files)
 };
