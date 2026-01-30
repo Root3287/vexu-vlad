139a8fa653cfa4b6df2e4c4c0a92469e8215dbb6
Rbussom <ryanbussom@gmail.com>
2026-01-30 17:09:45 -0600

    .env vars updated

--- backend/controllers/email.controller.js
+++ backend/controllers/email.controller.js
@@ -10,8 +10,8 @@ const SALT_ROUNDS = 12;
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
   },
   debug: true,
   logger: true
